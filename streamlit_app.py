ee2e1c158ef50478276c4c1c1d7f7822ae2f56b1
Mickael Kohler <kohler.mick@gmail.com>
2021-05-02 14:56:45 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -108,20 +108,20 @@ if categorie == 'Introduction':
     Bienvenu dans le Projet MORE.
     
     Ce projet a pour objectif de fournir les outils d’analyse d’une base de données de films, 
-    afin de comprend les **indices clés** et de vous aider à la prise de décision. 
+    afin d'en comprendre les **indices clés**. 
     
-    Les deux premières parties vous mettent à disposition des **données comparatives sur le monde du cinéma** 
+    Les deux premières parties mettent à disposition des **données comparatives sur le monde du cinéma** 
     en se concentrant sur la comparaison entre les meilleurs films et la moyenne des films 
     ou sur la place des femmes dans le cinéma. 
     
-    La section suivante va vous générer des **TOP 10**, de films, d’acteurs ou de réalisateurs, 
+    La section suivante génère des **TOP 10**, de films, d’acteurs et de réalisateurs, 
     compte tenu de critères que vous aurez déterminés (une décennie ou un genre). 
     
     Enfin, la dernière section met à profit la puissance du **Machine Learning** 
-    pour vous apporter les meilleures recommandations de films, 
-    que ce soit la recommandation de films proche qu’un film que vous avez aimé, 
+    pour apporter les meilleures recommandations de films, 
+    que ce soit la recommandation de films proches qu’un film selectionné, 
     la proposition de films dans le cadre d’une rétrospective et enfin, 
-    la probabilité que vous aimiez un films compte tenu d’une sélection de films que vous appréciez.
+    la probabilité d'aimé un films compte tenu d’une sélection de films.
     
     Il ne vous reste plus qu’à explorer les sections !   
     """)
@@ -130,7 +130,7 @@ if categorie == 'Introduction':
     st.subheader('I need MORE !')
     col1, col2, col3 = st.beta_columns(3)
     with col2 :
-        st.image('/Users/miko/Documents/Dev/sub.png')
+        st.image('https://github.com/MickaelKohler/Projet_MORE/raw/main/Ressources/sub.png')
 
 
 elif categorie == 'Analyse Comparative':
